ae072910af5943bc8ae2a53152efb6a87ff1c5b0
emoly <emdormody@mun.ca>
2022-12-07 04:31:35 -0330

    woah

---
+++ day7/day7.py
@@ -0,0 +1,40 @@
+file = open("day7.txt", "r")
+line = file.readline().split()
+
+path = []
+directories = [] #these two are basically the same, directories just does the math
+
+totals = []
+
+while line != []:
+    if line[0] == "$":
+        if line[1] == "cd":
+            if line[2] == "..":
+                current = path.pop()
+                totals.append(directories.pop())
+            else:
+                current = line[2]
+                directories.append(0)
+                path.append(current)
+    else:
+        if line[0] != "dir":
+            for x in range(len(directories)):
+                directories[x] += int(line[0])
+    line = file.readline().split()
+
+while directories != []:
+    totals.append(directories.pop())
+
+counter = 0
+for x in totals:
+    if x <= 100000:
+        counter += x
+
+print("part 1:", counter)
+
+#part2
+totals.sort()
+for y in totals:
+    if y >= 30000000 - (70000000 - max(totals)):
+        print("part 2:",y)
+        break
